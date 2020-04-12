3548c6948bf764617a4438550c618c49aadd1e52
Freddy Wicaksono <59552422+freddywicaksono@users.noreply.github.com>
2020-04-12 16:48:32 +0700

    Update MyMod.vb

--- MyMod.vb
+++ MyMod.vb
@@ -1,8 +1,8 @@
 Imports MongoDB.Driver
 Imports MongoDB.Bson
 Module MyMod
-    'Public dbClient = New MongoClient("mongodb+srv://Freddy:K6G5A7X6AM@clusterumc-gg1zy.mongodb.net/akademik?authSource=admin&replicaSet=ClusterUMC-shard-0&connectTimeoutMS=360000&socketTimeoutMS=360000&readPreference=primary&ssl=true")
-    Public dbClient = New MongoClient("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false")
+    
+    Public dbClient = New MongoClient("mongodb://localhost:27017/?readPreference=primary&ssl=false")
     Public db As IMongoDatabase = dbClient.GetDatabase("akademik")
 
     Public oMhs As MongoDB.Driver.IMongoCollection(Of MongoDB.Bson.BsonDocument)
